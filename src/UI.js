f66de3e0f20b6566387eae8e118691031a4b8a10
lukechopper <lukechopper22@gmail.com>
2021-09-08 16:16:07 +0100

    Scrolling balls

--- src/UI.js
+++ src/UI.js
@@ -1,58 +1,69 @@
 import {ctx, canvas} from './Utils/canvasInfo';
+import {GUIPath, ArcFill} from './UI/GUIObject';
 import {mousePos} from './Utils/mouseMovement';
 
 let currentFrameNum = 1;
 
 export function drawFrameNum(){
     ctx.font = '70px Arial';
-    ctx.fillText(currentFrameNum.toString(), 15, canvas.height - 20);
-}
-
-function makeWayForNum(){
+    ctx.fillStyle = 'black';
     let numOfDigits = currentFrameNum.toString().length;
-    return (numOfDigits - 1) * 40;
+    ctx.fillText(currentFrameNum.toString(), canvas.width - (60 + ((numOfDigits - 1) * 40)), 80);
 }
 
-export let checkPointTracker = []; //Format {checked, label}
+let checkPointTracker = []; //Format {checked, label}
+export function pushToCheckPointTracker(objToPush){
+    checkPointTracker.push(objToPush);
+}
+
+let numOfCircles = 24;
+let circlePos = 110, mouseDiffX = null, movedAway = 0;
+
+function moveUI(){
+    if(isMouseDown){
+        circlePos = mousePos.x - mouseDiffX;
+        if(circlePos > 110){
+            circlePos = 110;
+        }else if((circlePos + ((numOfCircles - 1) * 74)) < canvas.width - 110){
+            circlePos = (canvas.width - 110) - ((numOfCircles - 1) * 74);
+        }
+        movedAway = 110 - circlePos;
+    }
+}
+
 
 export function drawUi(){
-    ctx.save();
-    ctx.globalAlpha = 0.3;
-    ctx.beginPath(); //drawing left arrow
-    let sp = {x: 65 + makeWayForNum(), y: canvas.height - 50};
-    ctx.moveTo(sp.x, sp.y);
-    sp.x += 45, sp.y -= 30;  ctx.lineTo(sp.x, sp.y);
-    sp.y += 60; ctx.lineTo(sp.x, sp.y);
-    let inside = ctx.isPointInPath(mousePos.x, mousePos.y); checkPointTracker.push({checked: inside, label: 'leftGuiBtn'});
-    ctx.closePath();
-    if(inside){
-        ctx.fillStyle = 'gold'; ctx.globalAlpha = 0.5;
-    }else{
-        ctx.fillStyle = '#005ce6';
-    }
-    ctx.fill();
-    ctx.restore();
-    ctx.save();
-    ctx.globalAlpha = 0.3;
-    ctx.beginPath(); //drawing right arrow
-    sp = {x: canvas.width - 20, y: canvas.height - 50};
-    ctx.moveTo(sp.x, sp.y);
-    sp.x -= 45, sp.y -= 30;  ctx.lineTo(sp.x, sp.y);
-    sp.y += 60; ctx.lineTo(sp.x, sp.y);
-    inside = ctx.isPointInPath(mousePos.x, mousePos.y); checkPointTracker.push({checked: inside, label: 'rightGuiBtn'});
-    ctx.closePath();
-    if(inside){
-        ctx.fillStyle = 'gold'; ctx.globalAlpha = 0.5;
-    }else{
-        ctx.fillStyle = '#005ce6';
-    }
-    ctx.fill();
-    ctx.restore();
     //drawing circles
-    
+    let circleStore = [];
+    moveUI();
+    for(let i = 1; i <= numOfCircles; i++){
+        circleStore.push(new ArcFill({x: circlePos + ((i - 1) * 74), y: canvas.height - 50}, 30, 'circle-'+i));
+    }
+    circleStore.forEach((circle, i) => {
+        if(currentFrameNum === i + 1){
+            circle.selected = true;
+        }else{
+            circle.selected = false;
+        }
+        circle.draw()
+    }); 
+    ctx.beginPath();
+    ctx.clearRect(canvas.width - 65, canvas.height - 85, 80, 75); ctx.clearRect(0, canvas.height - 85, 65, 75);
+    const LeftArrow = new GUIPath({x: 20, y: canvas.height - 50}, [
+        {x: 45, y: -30},
+        {x: 0, y: 60}
+    ],'leftGuiBtn');
+    LeftArrow.draw();
+    const RightArrow = new GUIPath({x: canvas.width - 20, y: canvas.height - 50}, [
+        {x: -45, y: -30},
+        {x: 0, y: 60}
+    ], 'rightGuiBtn');
+    RightArrow.draw();
+    ctx.beginPath();
+    showScrollBar();
 }
 
-export function checkCheckPointTracker(){
+export function checkCheckPointTracker(){ //Check to see if cursor should be a pointer or not
     let haveFoundCheck = false;
     checkPointTracker.forEach(check => {
         if(haveFoundCheck) return;
@@ -65,10 +76,20 @@ export function checkCheckPointTracker(){
     });
 }
 
-export function clearCheckPointTracker(){
+export function clearCheckPointTracker(){ //Called at the start of repeated function
     checkPointTracker = [];
 }
 
+function showScrollBar(){
+    if(isThereCircleOverlap){
+        if(mousePos.x > 66 && mousePos.x < (canvas.width - 132) + 66 && mousePos.y > canvas.height - 85 &&
+                mousePos.y < (canvas.height - 85) + 75){
+                    ctx.fillRect(0,0,40,40);
+            }
+    }
+}
+
+let isMouseDown = false, isThereCircleOverlap = false;
 addEventListener('mousedown', e => {
     if(e.button === 0){
         let leftGuiBtn = checkPointTracker.find(check => check.label === 'leftGuiBtn');
@@ -77,7 +98,33 @@ addEventListener('mousedown', e => {
         }
         let rightGuiBtn = checkPointTracker.find(check => check.label === 'rightGuiBtn');
         if(rightGuiBtn.checked){
+            if(currentFrameNum + 1 > numOfCircles) numOfCircles++; //Need to render more circles to the GUI
             currentFrameNum++;
+        }
+        let regex = new RegExp('circle-', 'ig'), clickedCircle = checkPointTracker.find(check => check.checked && regex.test(check.label));
+        if(clickedCircle){
+            if(clickedCircle.canClick){
+                let number = Number(clickedCircle.label.split('-')[1]);
+                currentFrameNum = number;
+            }
+        }
+        //Check to see if they have clicked within the circle GUI area
+        if(isThereCircleOverlap){
+            if(mousePos.x > 66 && mousePos.x < (canvas.width - 132) + 66 && mousePos.y > canvas.height - 85 &&
+                mousePos.y < (canvas.height - 85) + 75){
+                    mouseDiffX = mousePos.x - (110 - movedAway);
+                    isMouseDown = true;
+            }
+        }
+    }
+});
+addEventListener('mouseup', e => {
+    if(e.button === 0){
+        isMouseDown = false;
+        if(!isThereCircleOverlap){
+            let regex = new RegExp('circle-', 'ig');
+            let findCircleOverlap = checkPointTracker.find(check => check.overlap && regex.test(check.label));
+            if(findCircleOverlap) isThereCircleOverlap = true;
         }
     }
 });
---
+++ src/UI/GUIObject.js
@@ -0,0 +1,66 @@
+import {ctx, canvas} from '../Utils/canvasInfo';
+import {pushToCheckPointTracker} from '../UI';
+import {mousePos} from '../Utils/mouseMovement';
+
+export function GUIPath(startingPoint, edges, label){
+    this.startingPoint = startingPoint;
+    this.edges = edges;
+    this.label = label;
+    this.draw = () => {
+        ctx.save();
+        ctx.globalAlpha = 0.3;
+        ctx.beginPath();
+        ctx.moveTo(this.startingPoint.x, this.startingPoint.y);
+        edges.forEach(edge => {
+            this.startingPoint.x += edge.x, this.startingPoint.y += edge.y;
+            ctx.lineTo(this.startingPoint.x, this.startingPoint.y);
+        });
+        let inside = ctx.isPointInPath(mousePos.x, mousePos.y);
+        pushToCheckPointTracker({checked: inside, label: this.label});
+        ctx.closePath();
+        if(inside){
+            ctx.fillStyle = 'gold'; ctx.globalAlpha = 0.5;
+        }else{
+            ctx.fillStyle = '#005ce6';
+        }
+        ctx.fill();
+        ctx.restore();
+    }
+}
+
+
+export function ArcFill(startingPoint, radius, label){
+    this.startingPoint = startingPoint;
+    this.radius = radius;
+    this.label = label;
+    this.selected = false;
+    this.draw = () => {
+        ctx.save();
+        ctx.globalAlpha = 0.3;
+        ctx.beginPath();
+        ctx.arc(this.startingPoint.x, this.startingPoint.y, this.radius, Math.PI * 2, false);
+        let inside = ctx.isPointInPath(mousePos.x, mousePos.y), overlap = false;
+        if((this.startingPoint.x - this.radius) < canvas.width - 65 && (this.startingPoint.x + this.radius) >  65){
+            if(mousePos.x < canvas.width - 65 && mousePos.x >  65){
+                pushToCheckPointTracker({checked: inside, label: this.label, overlap, canClick: true});
+            }else{
+                pushToCheckPointTracker({checked: inside, label: this.label, overlap, canClick: false});
+            }
+        }else{
+            overlap = true;
+            pushToCheckPointTracker({checked: false, label: this.label, overlap, canClick: true});
+        }
+        if(this.selected){
+            ctx.fillStyle = 'gold'; ctx.globalAlpha = 0.5;
+        }else{
+            if(inside){
+                if(mousePos.x < canvas.width - 65 && mousePos.x >  65){
+                    ctx.globalAlpha = 0.4;
+                }
+            }
+            ctx.fillStyle = '#005ce6';
+        }
+        ctx.fill();
+        ctx.restore();
+    }
+}
